9f5288a1cc2c6f24cba3bb1a6ea350efc2497d50
Alexey Kuleshevich <alexey@kuleshevi.ch>
2021-12-27 00:03:08 +0300

    Dump validity - it is maintenance nightmare

--- tests/Data/Conduit/AesonSpec.hs
+++ tests/Data/Conduit/AesonSpec.hs
@@ -6,26 +6,43 @@ module Data.Conduit.AesonSpec (spec) where
 import Conduit
 import Data.Aeson
 import Data.Conduit.Aeson
-import Data.GenValidity.Aeson ()
-import Data.GenValidity.Map ()
 import qualified Data.Map.Strict as Map
+import Data.Scientific
 import qualified Data.Text as T
 import GHC.Exts
 import Test.Hspec
-import Test.Validity
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
+
+instance Arbitrary T.Text where
+  arbitrary = T.pack <$> arbitrary
+
+instance Arbitrary Value where
+  arbitrary = sized sizedArbitraryValue
+
+sizedArbitraryValue :: Int -> Gen Value
+sizedArbitraryValue n
+  | n <= 0 = oneof [pure Null, bool, number, string]
+  | otherwise = resize n' $ oneof [pure Null, bool, number, string, array, object']
+  where
+    n' = n `div` 2
+    bool = Bool <$> arbitrary
+    number = Number <$> (scientific <$> arbitrary <*> arbitrary)
+    string = String <$> arbitrary
+    array = Array . fromList <$> arbitrary
+    object' = Object . fromList <$> arbitrary
 
 
 spec :: Spec
 spec = do
   describe "roundtrips" $ do
-    it "conduitArray" $ do
-      forAllValid $ \(xs :: [Value]) -> do
+    prop "conduitArray" $ \(xs :: [Value]) -> do
         xs' <-
           runConduit
             (sourceLazy (encode (Array (fromList xs))) .| conduitArray .| sinkList)
         xs' `shouldBe` xs
-    it "conduitObject" $ do
-      forAllValid $ \(xs :: Map.Map T.Text Value) -> do
+    prop "conduitObject" $ \(ls :: [(T.Text, Value)]) -> do
+        let xs = Map.fromList ls
         xs' <-
           runConduit
             (sourceLazy (encode (Object (fromList (Map.toList xs)))) .| conduitObject .| sinkList)
